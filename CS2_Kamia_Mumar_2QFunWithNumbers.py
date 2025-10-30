0d7e1b15377eb41aa9a9595504312e8113efc1b3
MarvinJacobPMumar <marvinjacob.mumar@smc.pshs.edu.ph>
2025-10-30 14:23:34 +0800

    Added FA2 (!)

---
+++ CS2_Kamia_Mumar_2QFunWithNumbers.py
@@ -0,0 +1,15 @@
+#Variables
+i = 0
+sum = 0
+
+#Input
+age = int(input("Hi there! Please enter your age: "))
+
+#Process
+while i < age:
+    i += 1
+    sum += i
+
+#Output
+print("The sum of all numbers from 1 to", age, "is:", sum)
+
